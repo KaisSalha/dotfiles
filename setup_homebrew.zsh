f32e56b46ebee7c20455ab222f8f1e2fd0a8ee23
Kais Salha <kaiss.salha@gmail.com>
2022-01-11 11:41:52 -0500

    brew path if installing

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -7,6 +7,8 @@ if exists brew; then
 else
     echo "Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/kaissalha/.zprofile 
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 brew bundle --verbose
